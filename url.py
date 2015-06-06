0c14ce7eb6abf94c0164dcbc19ae2b0c05aa6b6a
schteve <stevenheindel@gmail.com>
2015-06-06 07:28:42 -0400

    Minor cleanup

--- url.py
+++ url.py
@@ -19,22 +19,22 @@ base_file_name = 'puzzles/griddler_%d' % puzzle_id
 # If already exists on disk, just use that one. Otherwise grab from website and save to disk.
 
 disk_file_name = base_file_name + '.html'
-web_html = ""
+html = ''
 if os.path.isfile(disk_file_name):
    print "Reading HTML from disk."
    f = open(disk_file_name, 'r')
-   web_html = f.read()
+   html = f.read()
    f.close()
 else:
    print "Reading HTML from website."
    web_url_base = 'http://www.griddlers.net/griddlers?p_p_id=griddlers_WAR_puzzles&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=html&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_count=1&_griddlers_WAR_puzzles_id={0}&_griddlers_WAR_puzzles_view=detail'
    web_url = web_url_base.format(puzzle_id) # Add the puzzle ID into the url
    web_file = urllib2.urlopen(web_url)
-   web_html = web_file.read()
+   html = web_file.read()
    
    print "Writing HTML to disk."
    f = open(disk_file_name, 'w')
-   f.write(web_html)
+   f.write(html)
    f.close()
 print "Page data retrieved."
 
@@ -42,18 +42,18 @@ print "Page data retrieved."
 # Extract puzzle details
 
 # Puzzle width
-result = re.search('var pwidth = ([0-9]+)', web_html)
+result = re.search('var pwidth = ([0-9]+)', html)
 puzz_width = int(result.group(1))
 print "Puzzle width:", puzz_width
 
 # Puzzle height
-result = re.search('var pheight = ([0-9]+)', web_html)
+result = re.search('var pheight = ([0-9]+)', html)
 puzz_height = int(result.group(1))
 print "Puzzle height:", puzz_height
 
 # Puzzle horizontal (left) codes
 puzz_left_hints = []
-for result in re.finditer('leftCodes\[[0-9]+\]=\"(.*)\";', web_html): # todo: better regex use, can probably do this in one line
+for result in re.finditer('leftCodes\[[0-9]+\]=\"(.*)\";', html): # todo: better regex use, can probably do this in one line
    left_hint_list = []
    for r in result.group(1).split(','):
       left_hint = re.search('([0-9]+):([0-9]+)', r)
@@ -65,7 +65,7 @@ for result in re.finditer('leftCodes\[[0-9]+\]=\"(.*)\";', web_html): # todo: be
 
 # Puzzle vertical (top) codes
 puzz_top_hints = []
-for result in re.finditer('topCodes\[[0-9]+\]=\"(.*)\";', web_html):
+for result in re.finditer('topCodes\[[0-9]+\]=\"(.*)\";', html):
    top_hint_list = []
    for r in result.group(1).split(','):
       top_hint = re.search('([0-9]+):([0-9]+)', r)
@@ -76,19 +76,19 @@ for result in re.finditer('topCodes\[[0-9]+\]=\"(.*)\";', web_html):
    puzz_top_hints.append(top_hint_list)
 
 print "Column hints:"
-print '\n'.join([str(hint) for hint in puzz_top_hints]) # todo: try to use map for this?
-print "\n"
+print '\n'.join(map(str, puzz_top_hints))
+print ""
 
 print "Row hints:"
-print '\n'.join([str(hint) for hint in puzz_left_hints])
-print "\n"
+print '\n'.join(map(str, puzz_left_hints))
+print ""
 
 puzz_grid = grid.Grid(puzz_width, puzz_height, puzz_top_hints, puzz_left_hints)
 
 # Create puzzle grid
 #last_puzz_grid = copy_grid(puzz_grid)
 
-# Search possibilities
+# Solve it and time how long it takes
 begin_time = time.clock()
 puzz_grid.solve()
 end_time = time.clock()
@@ -111,8 +111,7 @@ with open(result_file_name, 'w') as fout:
    fout.write(str(puzz_grid))
 #raw_input("Press Enter to continue...")
 
-# - Could have a speedup where if the row is empty and the sum of the components isn't long enough,
-#   just skip the row and return all empties. I already have this but the speedup I have might be faulty.
+# todo:
 # - Create a queueing system for which rows / cols to look through. Initially add one or more that are
 #   very long. Then when a cell changes, add that row or col to the queue to be checked. If the queue is
 #   empty then revert to scanning through row x col.
